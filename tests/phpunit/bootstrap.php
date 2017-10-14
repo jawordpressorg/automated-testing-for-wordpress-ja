fb08ecd914f591d8d679e856e7a1cd71ea775e5e
miya0001 <miya@wpist.me>
2017-10-14 17:39:23 +0900

    set locale to ja

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -25,6 +25,9 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	global $_tests_dir;
+	define( 'WPLANG', 'ja' );
+	var_dump( get_locale() );
+	exit;
 	require $_tests_dir . '/data/wp-multibyte-patch/wp-multibyte-patch.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
