a7d3c025d659a94456afc150485bec09667840cc
miya0001 <miya@wpist.me>
2017-10-20 19:46:22 +0900

    remove regex

--- tests/phpunit/test-wp-multibyte-patch.php
+++ tests/phpunit/test-wp-multibyte-patch.php
@@ -98,7 +98,7 @@ class WP_Multibyte_Patch_Test extends WP_UnitTestCase
 	public function test_filename_should_be_sanitized_by_md5()
 	{
 		$filename = sanitize_file_name( '日本語.png' );
-		$this->assertRegexp( "/^[a-z0-9]{32}\.png$/", $filename );
+		$this->assertSame( "00110af8b4393ef3f72c50be5b332bec.png", $filename );
 
 		$dir = wp_upload_dir();
 		$upload_dir = $dir['basedir'];
@@ -106,7 +106,7 @@ class WP_Multibyte_Patch_Test extends WP_UnitTestCase
 
 		// The filename should be unique
 		$filename = wp_unique_filename( $upload_dir, $filename );
-		$this->assertRegexp( "/^[a-z0-9]{32}\-1\.png$/", $filename );
+		$this->assertSame( "00110af8b4393ef3f72c50be5b332bec-1.png", $filename );
 	}
 
 	/**
